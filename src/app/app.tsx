b1a14fb8acd44cc7b823c7bd3ee9f52ed0258701
camward <camward@yandex.ru>
2024-10-31 23:01:30 +0400

    базовый пример
    npm i -D json-server concurrently

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,11 +1,14 @@
-import './app.css'
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "../shared/api/query-client";
+import { TodoList } from "../modules/todo-list/todo-list";
 
 function App() {
   return (
-    <>
-      <p>app</p>
-    </>
-  )
+    <QueryClientProvider client={queryClient}>
+      <TodoList />
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
+
