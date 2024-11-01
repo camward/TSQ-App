7c94ad2c17d1c3b714d0459e0c41be414a0fd832
camward <camward@yandex.ru>
2024-11-01 22:11:48 +0400

    placeholderData и keepPreviousData

--- src/modules/todo-list/todo-list.tsx
+++ src/modules/todo-list/todo-list.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { todoListApi } from "./api";
 import { useState } from "react";
 
@@ -9,9 +9,11 @@ export function TodoList() {
     data: todoItems,
     error,
     isPending,
+    isPlaceholderData,
   } = useQuery({
     queryKey: ["tasks", "list", { page }],
     queryFn: (meta) => todoListApi.getTodoList({ page }, meta),
+    placeholderData: keepPreviousData,
   });
 
   if (isPending) {
@@ -26,7 +28,11 @@ export function TodoList() {
     <div className="p-5 mx-auto max-w-[1200px] mt-10">
       <h1 className="text-3xl font-bold underline mb-5"> Todo LIst</h1>
 
-      <div className="flex flex-col gap-4">
+      <div
+        className={
+          "flex flex-col gap-4" + (isPlaceholderData ? " opacity-50" : "")
+        }
+      >
         {todoItems.data.map((todo) => (
           <div className="border border-slate-300 rounded p-3" key={todo.id}>
             {todo.text}
