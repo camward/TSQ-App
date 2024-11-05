63a7bd0d45840a2d5c1a1670edae6ce4641bdc2b
camward <camward@yandex.ru>
2024-11-05 21:08:25 +0400

    pwa. offline-режим
    npm i @tanstack/query-sync-storage-persister @tanstack/react-query-persist-client vite-plugin-pwa

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -3,19 +3,35 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { App } from "./app.tsx";
 import "./index.css";
-import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Provider } from "react-redux";
 import { queryClient } from "../shared/api/query-client";
 import { store } from "../shared/redux";
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
+import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
+import { onlineManager } from "@tanstack/query-core";
+
+onlineManager.setOnline(navigator.onLine);
+
+const persister = createSyncStoragePersister({
+  storage: window.localStorage,
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <PersistQueryClientProvider
+      client={queryClient}
+      persistOptions={{ persister }}
+      onSuccess={() => {
+        queryClient.resumePausedMutations().then(() => {
+          queryClient.invalidateQueries();
+        });
+      }}
+    >
       <Provider store={store}>
         <App />
         <ReactQueryDevtools initialIsOpen={false} />
       </Provider>
-    </QueryClientProvider>
+    </PersistQueryClientProvider>
   </StrictMode>,
 );
